189799bf9716d817445a403fb1a7e1c21f2ecbd6
Seonghun Kim <kim.seonghun23@gmail.com>
2022-01-06 23:08:02 +0900

    add inapp CoreML model

---
+++ CoreMLSample/ViewController/TabBarController.swift
@@ -0,0 +1,36 @@
+//
+//  TabBarController.swift
+//  CoreMLSample
+//
+//  Created by Seonghun Kim on 2022/01/06.
+//
+
+import UIKit
+
+final class TabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let inAppViewController = ViewController()
+        inAppViewController.view.backgroundColor = .red
+        let inAppBarItem = UITabBarItem(
+            title: "InApp",
+            image: UIImage(systemName: "opticaldiscdrive"),
+            selectedImage: UIImage(systemName: "opticaldiscdrive.fill")
+        )
+        inAppViewController.tabBarItem = inAppBarItem
+        
+        let downloadViewController = ViewController()
+        downloadViewController.view.backgroundColor = .purple
+        let downloadBarItem = UITabBarItem(
+            title: "Download",
+            image: UIImage(systemName: "cloud"),
+            selectedImage: UIImage(systemName: "cloud.fill")
+        )
+        downloadViewController.tabBarItem = downloadBarItem
+    
+        setViewControllers([inAppViewController, downloadViewController], animated: false)
+        tabBar.backgroundColor = .white
+    }
+}
