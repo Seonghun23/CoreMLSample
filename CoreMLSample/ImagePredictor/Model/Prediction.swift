971772f1fa68cb06f1819ca7a6d447ed407a78b3
Seonghun Kim <kim.seonghun23@gmail.com>
2022-01-08 19:51:58 +0900

    add Prediction

---
+++ CoreMLSample/ImagePredictor/Model/Prediction.swift
@@ -0,0 +1,13 @@
+//
+//  Prediction.swift
+//  CoreMLSample
+//
+//  Created by Seonghun Kim on 2022/01/08.
+//
+
+import Foundation
+
+struct Prediction {
+    let classification: String
+    let confidencePercentage: String
+}
