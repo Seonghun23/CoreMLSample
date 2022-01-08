368c90e435714f4104c8246915d3b733806f6ee1
Seonghun Kim <kim.seonghun23@gmail.com>
2022-01-08 17:53:20 +0900

    add InAppModelImagePredictor

---
+++ CoreMLSample/ImagePredictor/InAppModelImagePredictor.swift
@@ -0,0 +1,12 @@
+//
+//  InAppModelImagePredictor.swift
+//  CoreMLSample
+//
+//  Created by Seonghun Kim on 2022/01/08.
+//
+
+import Foundation
+
+final class InAppModelImagePredictor: ImagePredictable {
+    
+}
